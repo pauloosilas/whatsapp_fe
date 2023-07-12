95b39d5669b2cfa4e05057137b217ab877a61da3
pauloosilas <paulo3356@gmail.com>
2023-07-12 19:50:36 -0300

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import { CallIcon, DocumentIcon } from './svg'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+
 function App() {
   return (
-    <div className="dark">
-      
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/login" element={<Login />}/>
+          <Route exact path="/register" element={<Register />}/>
+        </Routes>
+      </Router>
     </div>
+
   );
 }
 
