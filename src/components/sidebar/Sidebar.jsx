0a14f2fcaa1c8dc7fb4f5171195b2957e5e83239
pauloosilas <paulo3356@gmail.com>
2023-07-26 10:21:12 -0300

    search user

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,16 +2,27 @@ import { useState } from "react"
 import { Conversations } from "./conversations"
 import { SidebarHeader } from "./header/SidebarHeader"
 import { Notifications } from "./notifications"
-import { Search } from "./search"
+import { Search, SearchResults } from "./search"
 
 export const Sidebar = () => {
     const [searchResults, setSearchResults] = useState([])
+    console.log(searchResults)
   return (
    <div className="w-[40%] h-full select-none">
         <SidebarHeader />
         <Notifications />
-        <Search searchLength={searchResults.length}/>
-        <Conversations />
+        <Search 
+            searchLength={searchResults.length}
+            setSearchResults = {setSearchResults}/>
+        {
+          searchResults.length > 0 ? (
+          <>
+            <SearchResults searchResults={searchResults}/>
+          </>
+          ):(
+            <Conversations />
+          )
+        }
    </div>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -16,8 +16,8 @@ export default function Home() {
 
   
   return (
-   <div className="mim-h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
-      <div className="container min-h-screen flex">
+   <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
+      <div className="container h-screen flex">
         <Sidebar />
       </div>
    </div> 
